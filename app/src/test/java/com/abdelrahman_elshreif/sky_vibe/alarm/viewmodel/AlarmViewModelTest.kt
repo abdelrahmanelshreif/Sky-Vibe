df4efaf0bfba324d0ab2a3090f791a0c60f91d20
Abdelrahman-Elshreif <abdelrahman31009246@f-eng.tanta.edu.eg>
2025-04-06 07:08:26 +0200

    Sky-Vibe beta

--- app/src/test/java/com/abdelrahman_elshreif/sky_vibe/alarm/viewmodel/AlarmViewModelTest.kt
+++ app/src/test/java/com/abdelrahman_elshreif/sky_vibe/alarm/viewmodel/AlarmViewModelTest.kt
@@ -1,14 +1,20 @@
 package com.abdelrahman_elshreif.sky_vibe.alarm.viewmodel
 
 import androidx.work.WorkManager
+import com.abdelrahman_elshreif.sky_vibe.alarm.model.AlertType
+import com.abdelrahman_elshreif.sky_vibe.alarm.model.WeatherAlert
+import com.abdelrahman_elshreif.sky_vibe.alarm.model.WeatherAlertEvent
 import com.abdelrahman_elshreif.sky_vibe.data.repo.SkyVibeRepository
 import com.abdelrahman_elshreif.sky_vibe.utils.AlertScheduler
 import com.abdelrahman_elshreif.sky_vibe.utils.LocationUtilities
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import junit.framework.Assert.assertFalse
 import junit.framework.Assert.assertNotNull
 import junit.framework.Assert.assertNull
 import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 
@@ -26,37 +32,66 @@ class AlarmViewModelTest {
     }
 
     @Test
-    fun getAlertState_initialState() {
-        val initialState = viewModel.alertState.value
+    fun onEvent_OnAlertDeleted_callsRepositoryAndCancelsScheduler() = runTest {
+        val alert = WeatherAlert(
+            id = 99,
+            alertArea = "Alexandria",
+            startTime = 0,
+            endTime = 0,
+            type = AlertType.ALARM,
+            isEnabled = true,
+            description = "Alert",
+            latitude = 0.0,
+            longitude = 0.0
+        )
 
-        assertNotNull(initialState)
-        assertTrue(initialState.alerts.isEmpty())
-        assertFalse(initialState.isLoading)
-        assertNull(initialState.error)
+        viewModel.onEvent(WeatherAlertEvent.OnAlertDeleted(alert))
+
+        coVerify { repository.deleteAlert(alert) }
+        coVerify { alertScheduler.cancel(alert.id) }
+    }
+    @Test
+    fun initializeNotifications_setsNotificationsEnabledTrue() {
+        viewModel.initializeNotifications()
+        assertTrue(viewModel.notificationsEnabled.value)
     }
 
     @Test
-    fun getShowAddDialog() {
+    fun disableNotifications_setsNotificationsEnabledFalse() {
+        viewModel.initializeNotifications()
+        viewModel.disableNotifications()
+        assertFalse(viewModel.notificationsEnabled.value)
     }
 
     @Test
-    fun getNotificationsEnabled() {
+    fun onEvent_OnDismissDialog_hidesDialog() {
+        viewModel.onEvent(WeatherAlertEvent.OnDismissDialog)
+        assertFalse(viewModel.showAddDialog.value)
     }
 
     @Test
-    fun getLocationOnDemand() {
-    }
+    fun onEvent_OnSaveAlert_callsRepositoryAndScheduler() = runTest {
+        val alert = WeatherAlert(
+            id = 0,
+            alertArea = "",
+            startTime = System.currentTimeMillis() + 10_000,
+            endTime = System.currentTimeMillis() + 60_000,
+            type = AlertType.ALARM,
+            isEnabled = true,
+            description = "Test Alert",
+            latitude = 30.0,
+            longitude = 31.0
+        )
 
-    @Test
-    fun initializeNotifications() {
-    }
+        coEvery { repository.getSavedLocation() } returns Pair(30.0, 31.0)
+        coEvery { repository.getAddressFromLocation(30.0, 31.0) } returns "Test Address"
+        coEvery { repository.addNewAlert(any()) } returns 1L
 
-    @Test
-    fun disableNotifications() {
-    }
+        viewModel.onEvent(WeatherAlertEvent.OnSaveAlert(alert))
 
-    @Test
-    fun onEvent() {
+        coVerify { repository.addNewAlert(any()) }
+        coVerify { alertScheduler.schedule(any()) }
+        assertFalse(viewModel.showAddDialog.value)
     }
 }
 
